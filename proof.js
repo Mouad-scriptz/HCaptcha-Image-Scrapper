d230df9f3abac1d00ac59655bfd26d757a97cb4c
Mouad scriptz <102869034+Mouad-scriptz@users.noreply.github.com>
2023-09-29 19:25:15 +0100

    Create proof.js

---
+++ proof.js
@@ -0,0 +1,25 @@
+const fs      = require('fs')
+const { JSDOM, ResourceLoader } = require("jsdom");
+
+const hsw     = fs.readFileSync(__dirname + "/hsw.js", "utf-8");
+let userAgent = process.argv[3]
+
+const {window}  = new JSDOM(``, {
+    url                 : "https://discord.com",
+    referrer            : "https://discord.com",
+    contentType         : "text/html",
+    includeNodeLocations: false,
+    runScripts          : "outside-only",
+    pretendToBeVisual   : true,
+    resources           : new ResourceLoader({ userAgent })
+});
+
+_window_ = window;
+_window_.eval(hsw)
+
+token = process.argv[2]
+
+_window_.get_hsw(token).then(function (result) {
+    console.log(result)
+})
+
